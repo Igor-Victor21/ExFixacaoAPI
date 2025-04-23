1e04123961a0d6095b76dc2611c8324559fccf7a
Igor-Victor21 <igor.victorcontato@gmail.com>
2025-04-23 17:00:43 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import style from './App.module.css'
 import { Menu } from './components/menu'
 
+import trunks from './assets/images/trunks02.jpg'
+
 function App() {
 
   return (
     <>
     <Menu option01='Página inicial' option02='Personagens'/>
     <main>
-      <section id='s1' className={style.s1}>
-        <h1>pag 01</h1>
+       <section id='s1' className={style.s1}>
+        <img className={style.img}src={trunks} alt="trunks-ssj" width={200}/>
+        <div className={style.slogan}>
+          <h1>Trunks uma vez disse:</h1>
+          <p>- "Toma sua filha da puta, Só porque é gostosinha, Não fica se achando muito não"</p>
+        </div>
+        <button>Saíba mais</button>
       </section>
     </main>
     </>
